2161b4d91ff54a8df0340db8ce09b1730fcc2cf2
dillow191 <danieldoskil@gmail.com>
2024-09-22 20:57:25 -0300

    Recomaçando do zero

--- script/app.js
+++ script/app.js
@@ -11,23 +11,20 @@ function adivinhar()
             resultados += `
                 <section class="dicaContainer">
                     <div class="dica">
-                        <h2>${i.bandeira}</h2>
+                        <p>${i.bandeira}</p>
                     </div>   
                     <div class="dica">
-                        <h2>${i.nome}</h2>
+                        <p>${i.continente}</p>
                     </div>
                     <div class="dica">
-                        <h2>${i.idioma}</h2>
+                        <p>${i.letra}</p>
                     </div> 
                     <div class="dica">
-                        <h2>${i.hemisferio}</h2>
+                        <p>${i.idioma}</p>
                     </div>
                     <div class="dica">
-                        <h2>${i.continente}</h2>
+                        <p>${i.hemisferio}</p>
                     </div>  
-                    <div class="dica">
-                        <h2>${i.letra}</h2>
-                    </div>
                 </section>`  
         }     
         dica.innerHTML = resultados 
--- script/dados.js
+++ script/dados.js
@@ -14,83 +14,94 @@ let paises = [
     hemisferio: "Norte",
     bandeira: "bandeiras",
     letra: "C"
+  },
+  {
+    nome: "argentina",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Sul",
+    bandeira: "bandeiras/argentina.png",
+    letra: "A"
+  },
+  {
+    nome:"bolivia",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Sul",
+    bandeira: "bandeiras/bolivia.png",
+    letra: "B"
+  },
+  { 
+    nome: "chile",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Sul",
+    bandeira: "bandeiras/chile.png",
+    letra: "C"
+  },
+  {
+    nome: "equador",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Norte",
+    bandeira: "bandeiras/equador.png",
+    letra: "E"
+  },
+  {
+    nome:"guiana",
+    continente: "América do Sul",
+    idioma: "Inglês",
+    hemisferio: "Norte",
+    bandeira: "bandeiras/guiana.png",
+    letra: "G"
+  },
+  {
+    nome:"guiana francesa",
+    continente: "América do Sul",
+    idioma: "Francês",
+    hemisferio: "Norte",
+    bandeira: "bandeiras/guiana_francesa.png",
+    letra: "G"
+  },
+  {
+    nome:"paragu",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Sul",
+    bandeira: "bandeiras/paraguai.png",
+    letra: "P"
+  },
+  {
+    nome:"Peru",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Sul",
+    bandeira: "bandeiras/peru.png",
+    letra: "P"
+  },
+  {
+    nome:"suriname",
+    continente: "América do Sul",
+    idioma: "Neerlandês",
+    hemisferio: "Sul",
+    bandeira: "bandeiras/suriname.png",
+    letra: "S"
+    },
+  {
+    nome:"uruguai",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Sul",
+    bandeira: "bandeiras/uruguai.png",
+    letra: "U"
+  },
+  {
+    nome:"venezuela",
+    continente: "América do Sul",
+    idioma: "Espanhol",
+    hemisferio: "Norte",
+    bandeira: "bandeiras/venezuela.png",
+    letra: "V"
   }
-    // "Argentina": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Sul",
-    //   bandeira: "bandeiras/argentina.png",
-    //   letra: "A"
-    // },
-    // "Bolivia": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Sul",
-    //   bandeira: "bandeiras/bolivia.png",
-    //   letra: "B"
-    // },
-    // "Chile": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Sul",
-    //   bandeira: "bandeiras/chile.png",
-    //   letra: "C"
-    // },
-    // "Equador": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Norte",
-    //   bandeira: "bandeiras/equador.png",
-    //   letra: "E"
-    // },
-    // "Guiana": {
-    //   continente: "América do Sul",
-    //   idioma: "Inglês",
-    //   hemisferio: "Norte",
-    //   bandeira: "bandeiras/guiana.png",
-    //   letra: "G"
-    // },
-    // "Guiana Francesa": {
-    //   continente: "América do Sul",
-    //   idioma: "Francês",
-    //   hemisferio: "Norte",
-    //   bandeira: "bandeiras/guiana_francesa.png",
-    //   letra: "G"
-    // },
-    // "Paraguai": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Sul",
-    //   bandeira: "bandeiras/paraguai.png",
-    //   letra: "P"
-    // },
-    // "Peru": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Sul",
-    //   bandeira: "bandeiras/peru.png",
-    //   letra: "P"
-    // },
-    // "Suriname": {
-    //   continente: "América do Sul",
-    //   idioma: "Neerlandês",
-    //   hemisferio: "Sul",
-    //   bandeira: "bandeiras/suriname.png",
-    //   letra: "S"
-    // },
-    // "Uruguai": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Sul",
-    //   bandeira: "bandeiras/uruguai.png",
-    //   letra: "U"
-    // },
-    // "Venezuela": {
-    //   continente: "América do Sul",
-    //   idioma: "Espanhol",
-    //   hemisferio: "Norte",
-    //   bandeira: "bandeiras/venezuela.png",
-    //   letra: "V"
-    // }
 
 ]
